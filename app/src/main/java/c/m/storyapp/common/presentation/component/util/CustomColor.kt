00c06bd9046be98bc418c8540851a13f5e976bda
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2023-01-19 23:34:07 +0700

    create composable function for compose component adaptive custom color

---
+++ app/src/main/java/c/m/storyapp/common/presentation/component/util/CustomColor.kt
@@ -0,0 +1,17 @@
+package c.m.storyapp.common.presentation.component.util
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+/**
+ * this file is used to store custom color settings that will be used repeatedly in jetpack compose files.
+ */
+
+// If you need dynamic background color on dark theme and light theme for ui component
+@Composable
+fun AdaptiveCustomColor(isDarkTheme: Boolean, onDark: Color, onLight: Color): Color =
+    if (isDarkTheme) {
+        onDark
+    } else {
+        onLight
+    }
