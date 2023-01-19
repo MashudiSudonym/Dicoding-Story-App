17bd611d2a5ba6d9c8a0119edb9cd4c20657ffa0
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2023-01-19 22:58:02 +0700

    add login screen file

--- app/src/main/java/c/m/storyapp/login/presentation/screen/LoginScreen.kt
+++ app/src/main/java/c/m/storyapp/login/presentation/screen/LoginScreen.kt
@@ -1,2 +1,9 @@
 package c.m.storyapp.login.presentation.screen
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LoginScreen(modifier: Modifier) {
+
+}
