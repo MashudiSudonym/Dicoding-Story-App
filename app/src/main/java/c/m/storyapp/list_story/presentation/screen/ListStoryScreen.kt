62da1de37d49007ff11ed727321ba19d89202429
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2023-01-23 14:24:35 +0700

    add preview

--- app/src/main/java/c/m/storyapp/list_story/presentation/screen/ListStoryScreen.kt
+++ app/src/main/java/c/m/storyapp/list_story/presentation/screen/ListStoryScreen.kt
@@ -1,14 +1,28 @@
 package c.m.storyapp.list_story.presentation.screen
 
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import c.m.storyapp.common.presentation.navigation_graph.MainScreenNavGraph
+import c.m.storyapp.common.presentation.ui.theme.StoryAppTheme
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
 
 @MainScreenNavGraph(start = true)
 @Destination
 @Composable
 fun ListStoryScreen(navigator: DestinationsNavigator) {
     Text(text = "Hello World")
+}
+
+@Preview
+@Composable
+private fun ListStoryScreenDefaultPreview() {
+    StoryAppTheme {
+        Surface {
+            ListStoryScreen(EmptyDestinationsNavigator)
+        }
+    }
 }
