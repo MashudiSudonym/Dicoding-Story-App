29db02a8069d94a5cac82306233804292133483c
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2023-02-03 22:18:33 +0700

    add mixin field validation

---
+++ lib/common/util/field_validation_mixin.dart
@@ -0,0 +1,10 @@
+mixin FieldValidationMixin {
+  bool isPasswordValid(String password) => password.length >= 8;
+  bool isName(String name) => name.isNotEmpty;
+  bool isEmailValid(String email) {
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = RegExp(pattern.toString());
+    return regex.hasMatch(email);
+  }
+}
